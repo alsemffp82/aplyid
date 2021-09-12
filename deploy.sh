f299447ae83fc53f2ac753c924dde0d0c1c72b5d
Martin <martinchonz@gmail.com>
2021-09-12 17:15:25 +1200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:alsemffp82/aplyid.git main:master
+git push -f git@github.com:alsemffp82/aplyid.git main:gh-pages
 cd -
